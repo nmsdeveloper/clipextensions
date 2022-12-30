237271ba9692931f3f43eaa3930e9fcd99090806
NMSDeveloper <nmsdeveloper01@gmail.com>
2022-12-30 20:36:08 +0000

    Changes to be committed: 	modified:   assets/css/styles.css 	modified:   assets/js/main.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,38 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
+/*=============== Scroll Sections Active Link ===============*/
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (current.getAttribute("id") !== "products") {
+      const sectionsClass = document.querySelector(
+        ".nav-menu a[href*=" + sectionId + "]"
+      );
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+        sectionsClass.classList.add("active-link");
+      else sectionsClass.classList.remove("active-link");
+    } else {
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+        document
+          .querySelector(".nav-link-dropdown")
+          .classList.add("active-link");
+      else
+        document
+          .querySelector(".nav-link-dropdown")
+          .classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
 /*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
